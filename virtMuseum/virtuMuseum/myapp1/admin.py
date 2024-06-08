79645f5915045b4649ab6ee33ddf307062930dfb
smurfuk <arisha100807@gmail.com>
2024-06-08 18:11:07 +0300

    усталь уже

--- virtMuseum/virtuMuseum/myapp1/admin.py
+++ virtMuseum/virtuMuseum/myapp1/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from myapp1.models import gorodaGeroi
+from myapp1.models import gorodaSlavi
+from myapp1.models import gorodaDoblesti
 admin.site.register(gorodaGeroi)
+admin.site.register(gorodaSlavi)
+admin.site.register(gorodaDoblesti)
 # Register your models here.
--- virtMuseum/virtuMuseum/myapp1/models.py
+++ virtMuseum/virtuMuseum/myapp1/models.py
@@ -7,7 +7,28 @@ class gorodaGeroi(models.Model):
     bigGeroi=models.CharField(max_length=10000, blank=True)
     longGeroi=models.DecimalField(max_digits=9, decimal_places=6)
     latGeroi = models.DecimalField(max_digits=9, decimal_places=6)
-
     def __str__(self):
         return self.nameGeroi
+
+class gorodaSlavi(models.Model):
+    objects = None
+    rankSlavi = models.CharField(max_length=30, blank=True)
+    nameSlavi = models.CharField(max_length=30, blank=True)
+    smallSlavi = models.CharField(max_length=10000, blank=True)
+    bigSlavi = models.CharField(max_length=10000, blank=True)
+    longSlavi = models.DecimalField(max_digits=9, decimal_places=6)
+    latSlavi = models.DecimalField(max_digits=9, decimal_places=6)
+    def __str__(self):
+        return self.nameSlavi
+
+class gorodaDoblesti(models.Model):
+    objects = None
+    rankDoblesti = models.CharField(max_length=30, blank=True)
+    nameDoblesti = models.CharField(max_length=30, blank=True)
+    smallDoblesti = models.CharField(max_length=10000, blank=True)
+    bigDoblesti = models.CharField(max_length=10000, blank=True)
+    longDoblesti = models.DecimalField(max_digits=9, decimal_places=6)
+    latDoblesti = models.DecimalField(max_digits=9, decimal_places=6)
+    def __str__(self):
+        return self.nameDoblesti
 # Create your models here.
