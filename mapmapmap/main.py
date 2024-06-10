17699112d668ca2cf1c71ac91de831c68a2f0a69
anishakool <vbarbarik0104@gmail.com>
2024-06-10 18:22:50 +0300

    промежуточный результат

--- mapmapmap/main.py
+++ mapmapmap/main.py
@@ -6,92 +6,93 @@ goroda_geroi = folium.Map(
 )
 saint_petersburg = folium.map.FeatureGroup()
 saint_petersburg.add_child(
-    folium.features.CircleMarker(
-        [59.938676, 30.314494], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [59.938676, 30.314494], popup = "ЛЕНИНГРАД. 8 мая 1965 Санкт-Петербург получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 moscow = folium.map.FeatureGroup()
 moscow.add_child(
-    folium.features.CircleMarker(
-        [55.755819, 37.617644], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [55.755819, 37.617644], popup = "МОСКВА. 8 мая 1965 Москва получила почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 volgograd = folium.map.FeatureGroup()
 volgograd.add_child(
-    folium.features.CircleMarker(
-        [48.707067, 44.516975], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [48.707067, 44.516975], popup = "ВОЛГОГРАД. 8 мая 1965 Волгоград получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 kiev = folium.map.FeatureGroup()
 kiev.add_child(
-    folium.features.CircleMarker(
-        [50.450441, 30.523550], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [50.450441, 30.523550], popup = "КИЕВ. 8 мая 1965 Киев получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 odessa = folium.map.FeatureGroup()
 odessa.add_child(
-    folium.features.CircleMarker(
-        [46.484213, 30.731689], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [46.484213, 30.731689], popup = "ОДЕССА. 8 мая 1965 Одесса получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 sevastopol = folium.map.FeatureGroup()
 sevastopol.add_child(
-    folium.features.CircleMarker(
-        [44.616020, 33.524471], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [44.616020, 33.524471], popup = "СЕВАСТОПОЛЬ. 8 мая 1965 Севастополь получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 novorossisk = folium.map.FeatureGroup()
 novorossisk.add_child(
-    folium.features.CircleMarker(
-        [44.723771, 37.768813], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [44.723771, 37.768813], popup = "НОВОРОССИЙСК. 14 сентября 1973 Новороссийск получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 kerch = folium.map.FeatureGroup()
 kerch.add_child(
-    folium.features.CircleMarker(
-        [45.356219, 36.467378], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [45.356219, 36.467378], popup = "КЕРЧЬ. 14 сентября 1973 Керчь получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 minsk = folium.map.FeatureGroup()
 minsk.add_child(
-    folium.features.CircleMarker(
-        [53.902284, 27.561831], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [53.902284, 27.561831], popup = "МИНСК. 26 июня 1974 Минск получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 tula = folium.map.FeatureGroup()
 tula.add_child(
-    folium.features.CircleMarker(
-        [54.193122, 37.617348], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [54.193122, 37.617348], popup = "ТУЛА. 7 декабря 1976 Тула получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 murmansk = folium.map.FeatureGroup()
 murmansk.add_child(
     folium.features.Marker(
-         [68.970663, 33.074918], tooltip = 'Мурманск'
+         [68.970663, 33.074918], popup = "МУРМАНСК. 6 мая 1985 Мурманск получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 smolensk = folium.map.FeatureGroup()
 smolensk.add_child(
-    folium.features.CircleMarker(
-        [54.782495, 32.048054], radius = 5, tooltip = 'Смоленск', # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [54.782495, 32.048054],  popup = "СМОЛЕНСК. 6 мая 1985 Смоленск получил почетное звание города героя.", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 spisokgg = [moscow, saint_petersburg, volgograd, kiev, odessa, sevastopol, novorossisk, kerch, minsk, tula, murmansk, smolensk]
 # add the feature group to the map  (добавить группу объектов на карту)
 for i in spisokgg:
     goroda_geroi.add_child(i)
-goroda_geroi.save('name.html')
+goroda_geroi.save('gp.html')
 
 
 #################################################################################################
@@ -101,100 +102,100 @@ goroda_vs = folium.Map(
 )
 belgo = folium.map.FeatureGroup()
 belgo.add_child(
-    folium.features.CircleMarker(
-        [50.36, 36.36], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [50.595414, 36.587277],  popup = "БЕЛГОРОД. 27 апреля 2007 Белгород получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 kursk = folium.map.FeatureGroup()
 kursk.add_child(
-    folium.features.CircleMarker(
-        [51.43, 36.11], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [51.730846, 36.193015], popup = "КУРСК. 27 апреля 2007 Курск получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 orel = folium.map.FeatureGroup()
 orel.add_child(
-    folium.features.CircleMarker(
-        [52.58, 36.05], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [52.970756, 36.064358], popup = "ОРЁЛ. 27 апреля 2007 Орёл получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 vladikavkaz = folium.map.FeatureGroup()
 vladikavkaz.add_child(
-    folium.features.CircleMarker(
-        [43.01, 44.41], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [43.024616, 44.681771], popup = "ВЛАДИКАВКАЗ. 8 октября 2007 Владикавказ получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 
 malgo = folium.map.FeatureGroup()
 malgo.add_child(
-    folium.features.CircleMarker(
-        [43.3032, 44.3508], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [43.509318, 44.585723], popup = "МАЛГОБЕК. 8 октября 2007 Малгобек получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 
 rghev = folium.map.FeatureGroup()
 rghev.add_child(
-    folium.features.CircleMarker(
-        [56.1556, 34.1939], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [56.263017, 34.334419], popup = "РЖЕВ. 8 октября 2007 Ржев получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 elnya = folium.map.FeatureGroup()
 elnya.add_child(
-    folium.features.CircleMarker(
-        [54.34, 33.10], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [54.575911, 33.182743], popup = "ЕЛЬНЯ. 8 октября 2007 город Ельня получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 elec = folium.map.FeatureGroup()
 elec.add_child(
-    folium.features.CircleMarker(
-        [52.37, 38.28], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [52.621938, 38.500446], popup = "ЕЛЕЦ. 8 октября 2007 Елец получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 
 voroneg = folium.map.FeatureGroup()
 voroneg.add_child(
-    folium.features.CircleMarker(
-        [51.4018, 39.1238], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [51.660781, 39.200296],  popup = "ВОРОНЕЖ. 16 февраля 2008 Воронеж получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 
 luga = folium.map.FeatureGroup()
 luga.add_child(
-    folium.features.CircleMarker(
-        [58.44, 29.51], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [58.735207, 29.847945], popup = "ЛУГА. 5 мая 2008 Луга получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 
 polarn = folium.map.FeatureGroup()
 polarn.add_child(
-    folium.features.CircleMarker(
-        [69.1154, 33.2722], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [69.197402, 33.437235], popup = "ПОЛЯРНЫЙ. 5 мая 2008 Полярный получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 
 rost_na_don = folium.map.FeatureGroup()
 rost_na_don.add_child(
-    folium.features.CircleMarker(
-        [47.1426, 39.4238], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [47.222109, 39.718813], popup = "РОСТОВ-НА-ДОНУ. 5 мая 2008 Ростов-на-Дону получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 
 tuapse = folium.map.FeatureGroup()
 tuapse.add_child(
-    folium.features.CircleMarker(
-        [44.06, 39.05], radius = 5,  # широта и долгота Санкт-Петербурга
-        color = 'red', fill_color = 'Red'
+    folium.features.Marker(
+        [44.095245, 39.073382], popup = "ТУАПСЕ. 5 мая 2008 Туапсе получил почетное звание города воинской славы", radius = 5,  # широта и долгота Санкт-Петербурга
+        icon = folium.Icon(color = 'red')
     )
 )
 
